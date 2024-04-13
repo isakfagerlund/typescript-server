bed0d5a76d3c66487d6d0532b440faec8b2222da
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-04-13 19:02:09 +0200

    fix json

--- api/budget.ts
+++ api/budget.ts
@@ -1,7 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 import { NewBudget, budgets } from '../src/db/schema';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { VercelRequest } from '@vercel/node';
 
 export const config = {
   runtime: 'edge',
@@ -10,11 +9,11 @@ export const config = {
 const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
 const db = drizzle(sql);
 
-export async function POST(request: VercelRequest) {
-  const newBudget: NewBudget = request.body;
+export async function POST(request: Request) {
+  const newBudget: NewBudget = await request.json();
   const createdBudget = await db.insert(budgets).values(newBudget);
 
-  return new Response(JSON.stringify(createdBudget), {
+  return new Response(JSON.stringify(newBudget), {
     status: 201,
     headers: { 'Content-Type': 'application/json' },
   });
